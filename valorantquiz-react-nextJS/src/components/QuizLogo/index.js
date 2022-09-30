bb3029830dccf7118636a93820c5bcc739b12780
hta <agathamagnusg@hotmail.com>
2022-09-30 11:16:23 -0300

    react-nextjs

---
+++ valorantquiz-react-nextJS/src/components/QuizLogo/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Logo({ className }) {
+  return (
+    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Valorant_logo_-_pink_color_version.svg/2560px-Valorant_logo_-_pink_color_version.svg.png' className={className} width="130" height="80"/>
+  );
+}
+
+Logo.propTypes = {
+  className: PropTypes.string.isRequired,
+};
+
+const QuizLogo = styled(Logo)`
+  margin: auto;
+  display: block;
+  @media screen and (max-width: 500px) {
+    margin: 0;
+  }
+`;
+
+export default QuizLogo;
